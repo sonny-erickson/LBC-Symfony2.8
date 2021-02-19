006159069be8ae1fa6f015fdcd2fc33650b77462
Sonny <pet63910@hotmail.fr>
2021-02-19 16:14:32 +0100

    Registration form modificated with Town fiels added

---
+++ src/QuizzBundle/Form/RegistrationFormType.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace QuizzBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('ville');
+    }
+    public function getParent()
+    {
+        return BaseRegistrationFormType::class;
+    }
+
+}
